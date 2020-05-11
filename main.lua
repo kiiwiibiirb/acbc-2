a84dd13d4e3454be54316773a680390f018d2411
kiiwii <samcorbitt_@outlook.com>
2020-05-11 01:04:50 -0500

    filler main.lua don't ask

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print('This is just some filler for the actual program. I don\'t know what else to say so here I guess I\'ll give you a kiwi bird fact.\nThey can lay eggs one third the size of themselves! Isn\'t that huge?!', 0, 300)
+end
